508bac4c0df0719a59622608d72d5aa6bcab0328
Bahaa Alhagar <damrawyy@yahoo.com>
2018-07-31 17:56:08 +0200

    youtube uploader facade

---
+++ src/Facades/YoutubeUploader.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BahaaAlhagar\Youtube\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class YoutubeUploader extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'youtubeUploader';
+    }
+}
--- src/YoutubeUploader.php
+++ src/YoutubeUploader.php
@@ -2,7 +2,7 @@
 
 namespace BahaaAlhagar\YoutubeUploader;
 
-class SkeletonClass
+class YoutubeUploader
 {
     /**
      * Create a new Skeleton Instance
@@ -10,17 +10,5 @@ class SkeletonClass
     public function __construct()
     {
         // constructor body
-    }
-
-    /**
-     * Friendly welcome
-     *
-     * @param string $phrase Phrase to return
-     *
-     * @return string Returns the phrase passed in
-     */
-    public function echoPhrase($phrase)
-    {
-        return $phrase;
     }
 }
